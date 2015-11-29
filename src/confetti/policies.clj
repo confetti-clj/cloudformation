beb7c537b4c88394e1fab01122e617c4f0f0b3d7
Martin Klepsch <martinklepsch@googlemail.com>
2015-11-29 17:35:42 +0100

    fix #1; allow created user to invalidate cloudfront items

--- src/confetti/policies.clj
+++ src/confetti/policies.clj
@@ -8,10 +8,13 @@
    :action   actions
    :resource resources})
 
+;; Actions on CloudFront distributions can't be limited to specific resoures unfortunately
+;; http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/UsingWithIAM.html#CloudFront_ARN_Format
 (defn user-policy [bucket-ref]
   {:version "2012-10-17"
-   :statement [(allow-statement ["s3:ListBucket"] [(join "arn:aws:s3:::" bucket-ref)])
-                (allow-statement ["*"] [(join "arn:aws:s3:::" bucket-ref "/*")])]})
+   :statement [(allow-statement ["cloudfront:CreateInvalidation"] ["*"])
+               (allow-statement ["s3:ListBucket"] [(join "arn:aws:s3:::" bucket-ref)])
+               (allow-statement ["*"] [(join "arn:aws:s3:::" bucket-ref "/*")])]})
 
 (defn bucket-policy [bucket-ref]
   {:version "2012-10-17"
